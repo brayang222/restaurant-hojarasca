0c36e84b801b5bcdb2f38ba71104118480a07b03
brayang222 <116692605+brayang222@users.noreply.github.com>
2023-08-13 15:26:35 -0500

    align

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 var loader
-      function  loadnow(opacity) {
-          if (opacity <= 0 ) {
-              displaycontent()
-          }
-          else {
-              loader.style.opacity = opacity;
-              window.setTimeout(function() {
-                  loadnow(opacity -0.10)
-              }, 100)
-          }
-      }
-      function displaycontent() {
-          loader.style.display = 'none';
-          document.getElementById('content').style.display = 'block';
-      }
-      document.addEventListener("DOMContentLoaded", function(){
-          loader = document.getElementById('loader');
-          loadnow(3);
-      })
+function loadnow(opacity) {
+  if (opacity <= 0) {
+    displaycontent()
+  }
+  else {
+    loader.style.opacity = opacity;
+    window.setTimeout(function () {
+      loadnow(opacity - 0.10)
+    }, 100)
+  }
+}
+function displaycontent() {
+  loader.style.display = 'none';
+  document.getElementById('content').style.display = 'block';
+}
+document.addEventListener("DOMContentLoaded", function () {
+  loader = document.getElementById('loader');
+  loadnow(3);
+})
 
- // ******************************************************************
+// ******************************************************************
 
 const menu = document.querySelector(".hamburguesa");
 const navegacion = document.querySelector(".nav");
-const img =document.querySelectorAll('img');
-const btnTodos=document.querySelector('.Todos')
-const btnEnsaladas=document.querySelector('.ensaladas')
-const btnPasta=document.querySelector('.pasta')
-const btnPizza=document.querySelector('.pizza')
-const btnPostres=document.querySelector('.postres')
-const contenedorPlatillos=document.querySelector('.platillos')
+const img = document.querySelectorAll('img');
+const btnTodos = document.querySelector('.Todos')
+const btnEnsaladas = document.querySelector('.ensaladas')
+const btnPasta = document.querySelector('.pasta')
+const btnPizza = document.querySelector('.pizza')
+const btnPostres = document.querySelector('.postres')
+const contenedorPlatillos = document.querySelector('.platillos')
 document.addEventListener("DOMContentLoaded", () => {
-  eventos();  
+  eventos();
   platillos();
 });
 
@@ -55,26 +55,26 @@ const botonCerrar = () => {
   btnCerrar.textContent = "x";
   btnCerrar.classList.add("btn-cerrar");
   //while (navegacion.children[5]) {
- //  navegacion.removeChild(navegacion.children[5]);
+  //  navegacion.removeChild(navegacion.children[5]);
   //}
   navegacion.appendChild(btnCerrar);
   cerrarMenu(btnCerrar, overlay);
 };
 
-const observer = new IntersectionObserver((entries, observer)=>{
-  entries.forEach(entry=>{
-      if(entry.isIntersecting){
-          const imagen = entry.target;
-          imagen.src = imagen.dataset.src;
-          observer.unobserve(imagen);
-      }
-  }); 
-});     
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const imagen = entry.target;
+      imagen.src = imagen.dataset.src;
+      observer.unobserve(imagen);
+    }
+  });
+});
 
 
-img.forEach(imagen=>{
+img.forEach(imagen => {
 
-observer.observe(imagen);
+  observer.observe(imagen);
 });
 
 const cerrarMenu = (boton, overlay) => {
@@ -91,46 +91,46 @@ const cerrarMenu = (boton, overlay) => {
   };
 };
 
-const platillos =()=>{
-  let platillosArreglo =[];
-  const platillos=document.querySelectorAll('.platillo')
-  
-  platillos.forEach(platillo=> platillosArreglo=[...platillosArreglo, platillo])
+const platillos = () => {
+  let platillosArreglo = [];
+  const platillos = document.querySelectorAll('.platillo')
 
-  const ensaladas = platillosArreglo.filter(ensalada=> ensalada.getAttribute('data-platillo') ==='ensalada');
-  const pastas = platillosArreglo.filter(pasta=> pasta.getAttribute('data-platillo')==='pasta')
-  const pizzas = platillosArreglo.filter(pizza=> pizza.getAttribute('data-platillo')==='pizza')
-  const postres = platillosArreglo.filter(postre=> postre.getAttribute('data-platillo')==='postre')
+  platillos.forEach(platillo => platillosArreglo = [...platillosArreglo, platillo])
+
+  const ensaladas = platillosArreglo.filter(ensalada => ensalada.getAttribute('data-platillo') === 'ensalada');
+  const pastas = platillosArreglo.filter(pasta => pasta.getAttribute('data-platillo') === 'pasta')
+  const pizzas = platillosArreglo.filter(pizza => pizza.getAttribute('data-platillo') === 'pizza')
+  const postres = platillosArreglo.filter(postre => postre.getAttribute('data-platillo') === 'postre')
 
   mostrarPlatillos(ensaladas, pastas, pizzas, postres, platillosArreglo);
 }
 
-const mostrarPlatillos = (ensaladas, pastas, pizzas, postres, todos)=>{
-  btnEnsaladas.addEventListener('click', ()=>{
+const mostrarPlatillos = (ensaladas, pastas, pizzas, postres, todos) => {
+  btnEnsaladas.addEventListener('click', () => {
     limpiarHtml(contenedorPlatillos);
-    ensaladas.forEach(ensalada=>contenedorPlatillos.appendChild(ensalada));
-  }); 
-  btnPasta.addEventListener('click', ()=>{
-    limpiarHtml(contenedorPlatillos);
-    pastas.forEach(pasta=>contenedorPlatillos.appendChild(pasta));
+    ensaladas.forEach(ensalada => contenedorPlatillos.appendChild(ensalada));
   });
-  btnPizza.addEventListener('click', ()=>{
+  btnPasta.addEventListener('click', () => {
     limpiarHtml(contenedorPlatillos);
-    pizzas.forEach(pizza=>contenedorPlatillos.appendChild(pizza));
+    pastas.forEach(pasta => contenedorPlatillos.appendChild(pasta));
   });
-  btnPostres.addEventListener('click', ()=>{
+  btnPizza.addEventListener('click', () => {
     limpiarHtml(contenedorPlatillos);
-    postres.forEach(postre=>contenedorPlatillos.appendChild(postre));
+    pizzas.forEach(pizza => contenedorPlatillos.appendChild(pizza));
   });
-  btnTodos.addEventListener('click', ()=>{
+  btnPostres.addEventListener('click', () => {
     limpiarHtml(contenedorPlatillos);
-    todos.forEach(todo=>contenedorPlatillos.appendChild(todo));
+    postres.forEach(postre => contenedorPlatillos.appendChild(postre));
+  });
+  btnTodos.addEventListener('click', () => {
+    limpiarHtml(contenedorPlatillos);
+    todos.forEach(todo => contenedorPlatillos.appendChild(todo));
   });
 
 }
 
-const limpiarHtml =(contenedor)=>{
-  while(contenedor.firstChild){
+const limpiarHtml = (contenedor) => {
+  while (contenedor.firstChild) {
     contenedor.removeChild(contenedor.firstChild);
     /*Limpia el html para que sólo aparezca el boton seleccionado */
   }
@@ -140,11 +140,11 @@ const limpiarHtml =(contenedor)=>{
 /*...............................................................................................*/
 const btnCart = document.querySelector('.container-cart-icon');
 const containerCartProducts = document.querySelector(
-    '.container-cart-products'
-    );
+  '.container-cart-products'
+);
 
 btnCart.addEventListener('click', () => {
-    containerCartProducts.classList.toggle('hidden-cart')
+  containerCartProducts.classList.toggle('hidden-cart')
 });
 
 /* ************************* */
@@ -157,7 +157,7 @@ const productsList = document.querySelector('.platillos');
 // Variable de arreglos de producto
 let allProducts = [];
 
-const valorTotal = document.querySelector ('.total-pagar')
+const valorTotal = document.querySelector('.total-pagar')
 
 const countProducts = document.querySelector('#contador-productos')
 
@@ -166,79 +166,79 @@ const countProducts = document.querySelector('#contador-productos')
 const cartEmpty = document.querySelector('.cart-empty');
 const cartTotal = document.querySelector('.cart-total');
 productsList.addEventListener('click', e => {
-    if(e.target.classList.contains('btn-add-cart')) {
-        const product = e.target.parentElement;
-        const product2 = e.target.parentElement.parentElement;
-         // me muestra toda la extructura externa
-        const infoProduct = {
-            quantity: 1,
-            title: product2.querySelector('h2').textContent,
-            price: product.querySelector('p').textContent,
-            
-        }; //para obtener el contenido del texto
+  if (e.target.classList.contains('btn-add-cart')) {
+    const product = e.target.parentElement;
+    const product2 = e.target.parentElement.parentElement;
+    // me muestra toda la extructura externa
+    const infoProduct = {
+      quantity: 1,
+      title: product2.querySelector('h2').textContent,
+      price: product.querySelector('p').textContent,
 
-        const exits = allProducts.some(product => product.title === infoProduct.title) 
-        //vierifica si lo que contiene el title está 2 veces, devolverá un booleano
-        if (exits) {
-            const products = allProducts.map(product => {
-                if(product.title === infoProduct.title) {
-                    product.quantity++;
-                   
-                   
-                    return product;
-                }
-                else {
-                    return product;
-                }
-            });
-            allProducts = [...products];
+    }; //para obtener el contenido del texto
 
-        }else {
+    const exits = allProducts.some(product => product.title === infoProduct.title)
+    //vierifica si lo que contiene el title está 2 veces, devolverá un booleano
+    if (exits) {
+      const products = allProducts.map(product => {
+        if (product.title === infoProduct.title) {
+          product.quantity++;
 
-            allProducts =[...allProducts, infoProduct];
+
+          return product;
         }
+        else {
+          return product;
+        }
+      });
+      allProducts = [...products];
 
-        showHTML();
+    } else {
+
+      allProducts = [...allProducts, infoProduct];
     }
-    
+
+    showHTML();
+  }
+
 });
 //icon close
-rowProduct.addEventListener('click', e =>{
-    if(e.target.classList.contains('icon-close')){
-        const product = e.target.parentElement;
-        const title = product.querySelector('p').textContent;
+rowProduct.addEventListener('click', e => {
+  if (e.target.classList.contains('icon-close')) {
+    const product = e.target.parentElement;
+    const title = product.querySelector('p').textContent;
 
-        allProducts = allProducts.filter(product => product.title !== title);
-        showHTML();
-    }
+    allProducts = allProducts.filter(product => product.title !== title);
+    showHTML();
+  }
 });
 
 // Función html
 const showHTML = () => {
 
-    if (!allProducts.length) {
-		cartEmpty.classList.remove('hidden');
-		rowProduct.classList.add('hidden');
-		cartTotal.classList.add('hidden');
-	} else {
-		cartEmpty.classList.add('hidden');
-		rowProduct.classList.remove('hidden');
-		cartTotal.classList.remove('hidden');
-	}
-    //limpiar html
-    rowProduct.innerHTML = '';
+  if (!allProducts.length) {
+    cartEmpty.classList.remove('hidden');
+    rowProduct.classList.add('hidden');
+    cartTotal.classList.add('hidden');
+  } else {
+    cartEmpty.classList.add('hidden');
+    rowProduct.classList.remove('hidden');
+    cartTotal.classList.remove('hidden');
+  }
+  //limpiar html
+  rowProduct.innerHTML = '';
 
-    let total = 0;
-    let totalOfProduct = 0;
+  let total = 0;
+  let totalOfProduct = 0;
 
 
-    allProducts.forEach(product => {
-        const containerProduct = document.createElement('div');
-        containerProduct.classList.add('cart-product');
+  allProducts.forEach(product => {
+    const containerProduct = document.createElement('div');
+    containerProduct.classList.add('cart-product');
 
-        containerProduct.innerHTML = 
-        
-        `<div class="info-cart-product">
+    containerProduct.innerHTML =
+
+      `<div class="info-cart-product">
         <span class="cantidad-producto-carrito">${product.quantity}</span>
         <p class="titulo-producto-carrito">${product.title}</p>
         <span class="precio-producto-carrito">$${product.quantity * product.price.slice(1)}</span>
@@ -259,22 +259,15 @@ const showHTML = () => {
     </svg>`;
 
     rowProduct.append(containerProduct);
-    
-        total = total + parseInt(product.quantity * product.price.slice(1));
-        //el slice sirve para quitar el signo de dólar, así sólo devuelve el número
-        totalOfProduct = totalOfProduct + product.quantity;
 
-        
-        
-    });
+    total = total + parseInt(product.quantity * product.price.slice(1));
+    //el slice sirve para quitar el signo de dólar, así sólo devuelve el número
+    totalOfProduct = totalOfProduct + product.quantity;
 
-        
-        countProducts.innerText = totalOfProduct;
-        
-        valorTotal.innerText = `$${total}`;
-        
-    
-}; 
+  });
+  countProducts.innerText = totalOfProduct;
+  valorTotal.innerText = `$${total}`;
+};
 
 // DARK MODE ***************************************
 
