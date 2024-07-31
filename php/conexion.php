43ebd4e5b0cf120b3c77178004ce73b16eb4a940
brayang222 <brayangomez521@gmail.com>
2024-07-31 10:59:59 -0500

    conexion

--- php/conexion.php
+++ php/conexion.php
@@ -1,11 +1,18 @@
 <?php
 
-$server = "localhost";
-$user = "root";
-$pass = "";
-$db = "restaurante";
+// $server = "localhost";
+// $user = "root";
+// $pass = "";
+// $db = "restaurante";
+// $port = '3306';
 
-$conn = new mysqli($server, $user, $pass, $db);
+$server = "ewr1.clusters.zeabur.com";
+$user = "root";
+$pass = "NBAPhCw39Jv2687Li0Tdk1W5oFylnO4X";
+$db = "restaurante";
+$port = '32606';
+
+$conn = new mysqli($server, $user, $pass, $db, $port);
 
 if($conn->connect_error) {
   die("conexion fallida" . $conn->connect_error);
